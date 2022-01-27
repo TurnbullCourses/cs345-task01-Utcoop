aa5e14955a14fc1bae827fac4d2d71ff58f32d14
Utcoop <jacksonbckr13@gmail.com>
2022-01-27 15:09:10 -0500

    Added More test cases to isEmailValidTest()

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -24,12 +24,15 @@ class BankAccountTest {
 
     @Test
     void isEmailValidTest(){
-        assertTrue(BankAccount.isEmailValid( "a@b.com"));
-        assertFalse( BankAccount.isEmailValid(""));
-        assertFalse(BankAccount.isEmailValid("abc-@mail.com"));
-        assertFalse(BankAccount.isEmailValid("abc#def@mail.com"));
-        assertFalse(BankAccount.isEmailValid("abc.def@mail#archive.com"));
-        assertTrue(BankAccount.isEmailValid( "abc_def@mail.com"));
+        assertTrue(BankAccount.isEmailValid( "a@b.com")); // Valid Email Address
+        assertFalse( BankAccount.isEmailValid("")); //Empty String
+        assertFalse(BankAccount.isEmailValid("abc-@mail.com")); // No special characters are permitted just before the @ symbol
+        assertFalse(BankAccount.isEmailValid("abc#def@mail.com")); // The character # is not permitted in a valid email address before the @ 
+        assertFalse(BankAccount.isEmailValid("abc.def@mail'archive.com")); // The character ' is not permitted in a valid email address after the @
+        assertFalse(BankAccount.isEmailValid("abcd@mail.c")); // There must be two characters in the suffix of the domain
+        assertTrue(BankAccount.isEmailValid("abcd@mail.cc")); // Valid Email Address
+        assertTrue(BankAccount.isEmailValid( "abc_def@mail.com")); // Valid Email Address
+        assertFalse(BankAccount.isEmailValid(".abcd@mail.com")); // Cannot start with a special character
 
     }
 
